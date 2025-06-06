2737dc40ce9e0ec109527ed92d7ad2f971ce1473
yanjinqiang <yanjinqiang@zofund.com>
2025-06-06 14:29:54 +0800

    chore: 删除http中无用的代码文件

--- src/core-tools/http/index.ts
+++ src/core-tools/http/index.ts
@@ -1,12 +1,9 @@
 import httpEnhancer from "./httpEnhancer";
 import parameter from "./httpPlug/parameter";
-import { httpPromiseAll, httpPromiseStatusWrap } from "./httpHelp";
-//import { openLoading, closeLoading } from "./httpPlug/loading";
 import response from "./httpPlug/response";
 import responseError from "./httpPlug/responseError";
 import session from "./httpPlug/session";
 import networkError from "./httpPlug/networkError";
-// import { getServerUrl } from "@Src/config";
 
 //根据不同的环境设置不同的服务器环境
 const serverUrlPrefix = "https://mobile.qiangungun.com/v1";
@@ -14,17 +11,9 @@ const serverUrlPrefix = "https://mobile.qiangungun.com/v1";
 const http = new httpEnhancer(serverUrlPrefix, "http://dev-yapi.gungunqian.cn:3000/mock/37");
 
 http.addBeforePlug(parameter)
-    //.addBeforePlug(openLoading)
     .addAfterPlug(response)
     .addAfterPlug(responseError)
     .addAfterPlug(session)
     .addErrorPlug(networkError)
-    //.addFinallyPlug(closeLoading);
 
 export default http;
-
-//promise.all处理
-export const promiseAll = httpPromiseAll;
-
-//promise.promiseStatusWrap 状态处理
-export const promiseStatusWrap = httpPromiseStatusWrap;
